8d1d959dbea8ded02433e8c86dd06150cb679097
savvoff <vigornom@gmail.com>
2021-04-27 00:23:48 +0300

    remove min prefix + add order page

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -56,14 +56,14 @@ gulp.task("styles", () => {
         })
       )
     )
-    .pipe(
-      gulpif(
-        production,
-        rename({
-          suffix: ".min"
-        })
-      )
-    )
+    // .pipe(
+    //   gulpif(
+    //     production,
+    //     rename({
+    //       suffix: ".min"
+    //     })
+    //   )
+    // )
     .pipe(plumber.stop())
     .pipe(gulpif(!production, sourcemaps.write("./maps/")))
     .pipe(gulp.dest(paths.styles.dist))
